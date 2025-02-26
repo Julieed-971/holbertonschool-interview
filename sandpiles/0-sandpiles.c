714464775f6566f49a104c6c7e6a277661704c9b
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-02-26 11:08:43 +0100

    Initial files

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sandpiles_sum - compute two sandpiles sum
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ */
+
+ void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+ {
+    
+ }
