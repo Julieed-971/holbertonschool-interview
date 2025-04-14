1b0c4f25236969a8dbdc5fa9b3347ae6538dd5aa
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-04-14 10:13:50 +0200

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,10 +7,6 @@ the goal is to check if all boxes can be unlocked starting from the first box.
 
 def canUnlockAll(boxes):
     """Determines if all the boxes of a list of list can be open"""
-    # Initialize a variable to track if all boxes can be opened
-    all_boxes_can_be_opened = True
-
-    # outer loop
     for i in range(len(boxes) - 1):
         if i + 1 not in boxes[i]:
             return False
