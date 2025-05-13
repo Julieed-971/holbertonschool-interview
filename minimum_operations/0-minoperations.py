2dad6a525546894e69b2fa970a2333dc44d24699
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-05-13 19:08:29 +0200

    Minimum Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Module to calculate the fewest number of operations 
+"""Module to calculate the fewest number of operations
 needed to result in exactly n H characters in the file"""
+
 
 def minOperations(n):
     if n == 1:
@@ -17,16 +18,16 @@ def minOperations(n):
         count_operations = 0
         char = 1
         cache = char
-        while char < n:
-            if (n % (cache * 2) == 0 and n/cache >= 2):
-                cache = char
-                char += cache
-                count_operations += 2
-            elif (n % char == 0 and (char + cache) <= n):
-                    cache = char
-                    char += cache
-                    count_operations += 2
-            else:
-                char += cache
-                count_operations += 1
+    while char < n:
+        if (n % (cache * 2) == 0 and n/cache >= 2):
+            cache = char
+            char += cache
+            count_operations += 2
+        elif (n % char == 0 and (char + cache) <= n):
+            cache = char
+            char += cache
+            count_operations += 2
+        else:
+            char += cache
+            count_operations += 1
     return count_operations
