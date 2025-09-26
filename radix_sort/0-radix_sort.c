74627f982997a5c62d0d766a964f9c089fc6dd59
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-09-26 14:56:45 +0200

    Initial commit

---
+++ radix_sort/0-radix_sort.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * radix_sort - Sorts an array of integer in ascending order
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void radix_sort(int *array, size_t size)
+{
+/* A list/array does not need to be sorted
+if its size is less than 2. */
+
+/* Find largest element in the array */
+
+
+}
