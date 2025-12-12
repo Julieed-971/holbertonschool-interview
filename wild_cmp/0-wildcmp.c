9d9d3552bfd622dc0d8d73e3f6b52703ccf889fb
Julie Dedieu <julie.dedieu1@gmail.com>
2025-12-12 12:53:18 +0100

    Wildcard compare

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * wildcmpRecursion - recursively compares two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string to compare
+ * @n: size of the first string
+ * @m: size of the second string
+ * Return: 1 if identical else 0
+ */
+
+int wildcmpRecursion(char *s1, char *s2, int n, int m)
+{
+	/* Check if both strings are empty*/
+	if (s1[n] == '\0' && s2[m] == '\0')
+	{
+		return (1);
+	}
+	/* Empty text can match with a pattern consisting of '*' only. */
+	if (s1[n] == '\0')
+	{
+		if (s2[m] == '*')
+		{
+			return (wildcmpRecursion(s1, s2, n, m + 1));
+		return (0);
+		}
+	}
+	if (s2[m] == '*')
+	{
+		return (wildcmpRecursion(s1, s2, n, m + 1) ||
+				wildcmpRecursion(s1, s2, n + 1, m));
+	}
+	if (s1[n] == s2[m])
+	{
+		return (wildcmpRecursion(s1, s2, n + 1, m + 1));
+	}
+	return (0);
+}
+/**
+ * wildcmp - compares two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string to compare
+ * Return: 1 if identical else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int n = 0;
+	int m = 0;
+
+	return (wildcmpRecursion(s1, s2, n, m));
+}
---
+++ wild_cmp/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+int wildcmp(char *s1, char *s2);
+int wildcmpRecursion(char *s1, char *s2, int n, int m);
+
+#endif /* HOLBERTON_H */
