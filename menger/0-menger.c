c0c9a8edab852e9b426c105da2ec08d20c84a548
Julie Dedieu <julie.dedieu1@gmail.com>
2026-03-21 19:45:33 +0100

    Initial commit

---
+++ menger/0-menger.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "0-menger.h"
+
+/**
+ * menger - draws a 2D Menger Sponge
+ *
+ * @level: level of the menger sponge to draw
+ */
+void menger(int level)
+{
+	if (level < 0)
+	{
+		return;
+	}
+
+	
+}
